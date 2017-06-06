4e8c30e2cf7a416ebe9fd931b5c1f64c6aec04a5
Andreas Schmid <andreas.schmid.as3@roche.com>
2017-06-06 11:40:22 +0200

    Add network lokkit, implement deployment of 3 rentables

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,7 +2,9 @@ var Rentable = artifacts.require("./Rentable.sol");
 var RentableDiscovery = artifacts.require("./RentableDiscovery.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Rentable, 'leDescription', 'leLocation', 7, 500);
+  deployer.deploy(Rentable, 'Locker 1', 'Top', 5, 1000);
+  deployer.deploy(Rentable, 'Locker 2', 'Middle', 10, 500);
+  deployer.deploy(Rentable, 'Locker 3', 'Bottom', 15, 200);
   deployer.link(Rentable, RentableDiscovery);
   deployer.deploy(RentableDiscovery);
 };
--- truffle.js
+++ truffle.js
@@ -4,6 +4,11 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    lokkit: {
+      host: "master.lokkit.io",
+      port: 8545,
+      network_id: "*" // Match any network id
     }
   }
 };
