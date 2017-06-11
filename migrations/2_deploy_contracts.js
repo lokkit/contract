e16681fc4dfaa3e317c7d1485432636e8523d574
Andreas Schmid <andreas.schmid.as3@roche.com>
2017-06-11 10:28:18 +0200

    Remove DentableDiscovery

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,7 @@
 var Rentable = artifacts.require("./Rentable.sol");
-var RentableDiscovery = artifacts.require("./RentableDiscovery.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Rentable, 'Locker 1', 'Top', 5, 1000);
   deployer.deploy(Rentable, 'Locker 2', 'Middle', 10, 500);
   deployer.deploy(Rentable, 'Locker 3', 'Bottom', 15, 200);
-  deployer.link(Rentable, RentableDiscovery);
-  deployer.deploy(RentableDiscovery);
 };
