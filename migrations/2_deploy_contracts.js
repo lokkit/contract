ce86f4e86b92d39fa709f37124cc982e04419ff1
Dominik Hirzel <hirzeldominik@gmail.com>
2017-06-24 18:19:35 +0200

    Adjust prices to be suitable for demo purposes

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,7 @@
 var Rentable = artifacts.require("./Rentable.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Rentable, 'Locker 1', 'Top', 5, 1000);
-  deployer.deploy(Rentable, 'Locker 2', 'Middle', 10, 500);
-  deployer.deploy(Rentable, 'Locker 3', 'Bottom', 15, 200);
+  deployer.deploy(Rentable, 'Locker 1', 'Top',   10000000000000000, 3000000000000000000);
+  deployer.deploy(Rentable, 'Locker 2', 'Middle', 5000000000000000, 2000000000000000000);
+  deployer.deploy(Rentable, 'Locker 3', 'Bottom', 3500000000000000, 1250000000000000000);
 };
